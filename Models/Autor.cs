f1e9c32ebd893674f99f25c46276cb834b4ae77f
David Voinescu <david.voinescu20030616@gmail.com>
2024-01-25 16:36:04 +0200

    Examen

--- Models/Autor.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Test_Examen.Models
-{
-    public class Autor
-    {
-        public int AutorId { get; set; }
-        public string Nume { get; set; }
-        public List<Carte> Carti { get; set; }
-    }
-}
--- Models/Editura.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Test_Examen.Models
-{
-    public class Editura
-    {
-        public int EdituraId { get; set; }
-        public string Nume { get; set; }
-        public List<Carte> Carti { get; set; }
-    }
-}
